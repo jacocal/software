90288f3ccf1010091123af61f0b62091f38698de
Jacobo Calderón <cacovolt@gmail.com>
2016-09-09 12:52:27 +0000

    Examen primer parcial terminado

--- examen/primerParcial/Sucursal.hpp
+++ examen/primerParcial/Sucursal.hpp
@@ -4,10 +4,8 @@ class Sucursal {
 protected:
     std::string nombre;
     std::string ingredientes[];
-    int currentElement = 0;
 public:
     
-    virtual void agregar(std::string pastel) = 0;
     virtual Sucursal* clonar() = 0;
     Sucursal(std::string nombre) : nombre(nombre) { }
 };
